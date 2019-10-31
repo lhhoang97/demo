da3db7c1406d56cb647589cb660f4cadc0b8f7f6
lhhoang97 <hoanglehuy2211@gmail.com>
2019-10-31 22:34:55 +0700

    hoang

---
+++ test.js
@@ -0,0 +1,16 @@
+class Number {
+  tinhgiaithua(arr) {
+    let result = 1;
+    for (let i = 1; i <= arr; i++) {
+      result = result * i;
+    }
+    console.log(result);
+  }
+  tinh(){
+
+
+    
+  }
+}
+
+module.exports = Number;
